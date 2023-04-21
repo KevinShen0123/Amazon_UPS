dea6ad5b1e33d40134295104d56759974a3b404a
ks713 <ks713@duke.edu>
2023-04-21 14:57:12 -0400

    m

--- back-server/interact.cpp
+++ back-server/interact.cpp
@@ -1,7 +1,21 @@
 #include  "querys.hpp"
 #include "socketutil.hpp"
 #include "bufferProcessing.hpp"
+#include "world_ups.pb.h"
 int main(){
-    std::cout<<"hello world"<<std::endl;
-    return 0;
+    int world_socket=build_client("127.0.0.1","12345");
+    std::vector<UInitTruck> us;
+   UConnect uc=uConnect(false,1,us,false);
+   std::string uconnectinfo=uc.SerializeAsString();
+   int status=send(world_socket,uconnectinfo.c_str(),uconnectinfo.length(),0);
+   if(status==-1){
+    std::cerr<<"socket error!!!!!!"<<std::endl;
+    exit(1);
+   }
+   char* world_message=new char[100];
+   int recvstatus=recv(world_socket,world_message,100,0);
+   std::string connectedMessage=std::string(world_message,100);
+   UConnected ucd;
+   ucd.ParseFromString(connectedMessage);
+   std::cout<<"ucd world id: "<<ucd.worldid()<<"ucd result is: "<<ucd.result()<<std::endl;
 }
